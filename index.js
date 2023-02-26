6da44bafbf3d67c123a44a166fefddabb1bf1316
Casimova Marina <maracasimova@gmail.com>
2023-02-26 15:03:55 +0200

    task1

---
+++ index.js
@@ -0,0 +1,12 @@
+// /*
+// Завдання 1
+// Нажатие на кнопку "SHOW ME" должно выводить значения из 
+// поля введения (смотрите на элементы в html-разметки)
+// */
+// const showMeBtnEl = document.querySelector("#alertButton");
+// const inputEl = document.querySelector("#alertInput");
+
+// showMeBtnEl.addEventListener('click', () => {
+//     alert(inputEl.value)
+    
+// })
